1fdd729a75ba82cdf2ab64096f141c9b66db5828
Evan Lucas <evanlucas@me.com>
2015-05-28 09:56:50 -0500

    Fix incorrect solution for remove exercise
    Closes #2

--- exercises/remove/solution/solution.js
+++ exercises/remove/solution/solution.js
@@ -5,7 +5,7 @@ mongo.connect(url, function(err, db) {
   if (err) throw err
   var collection = db.collection(process.argv[2])
   collection.remove({
-    _id: process.argv[2]
+    _id: process.argv[3]
   }, function(err) {
     if (err) throw err
     db.close()
