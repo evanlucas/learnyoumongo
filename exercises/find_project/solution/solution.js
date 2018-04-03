e94adcbcc1a60cbf3b45a95800c45ba10829cc2e
Brandon Murch <brandon.murch@protonmail.com>
2018-04-03 15:10:40 +1000

    Updating solution for find-project
    Solution now works with the most recent version of MongoDB

--- exercises/find_project/solution/solution.js
+++ exercises/find_project/solution/solution.js
@@ -3,20 +3,21 @@ var age = process.argv[2]
 
 var url = 'mongodb://localhost:27017/learnyoumongo'
 
-mongo.connect(url, function(err, db) {
+mongo.connect(url, function(err, client) {
   if (err) throw err
+  var db = client.db('learnyoumongo')
   var parrots = db.collection('parrots')
   parrots.find({
     age: {
       $gt: +age
     }
-  }, {
+  }).project({
     name: 1
   , age: 1
   , _id: 0
   }).toArray(function(err, docs) {
     if (err) throw err
     console.log(docs)
-    db.close()
+    client.close()
   })
 })
